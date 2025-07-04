817a2c0eff19460b4dc9e65298ca8fbe05cabbac
Krish-jswl <imkrishjaiswal05@gmail.com>
2025-07-04 09:44:13 +0000

    nvim config update

--- .config/nvim/lua/krish/plugins/colorscheme.lua
+++ .config/nvim/lua/krish/plugins/colorscheme.lua
@@ -15,17 +15,43 @@
 -- }
 
 --SOLARIZED_OSAKA
+-- return {
+--   {
+--     "craftzdog/solarized-osaka.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {
+--       transparent = true,
+--     },
+--     config = function(_, opts)
+--       require("solarized-osaka").setup(opts)
+--       vim.cmd.colorscheme("solarized-osaka")
+--     end,
+--   },
+-- }
+
+-- TokyoNight
 return {
   {
-    "craftzdog/solarized-osaka.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      transparent = true,
+      style = "storm", -- Options: "storm", "night", "moon", "day"
+      transparent = true, -- Enable transparent background
+      terminal_colors = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        sidebars = "transparent", -- Transparent sidebars
+        floats = "transparent", -- Transparent floating windows
+      },
     },
     config = function(_, opts)
-      require("solarized-osaka").setup(opts)
-      vim.cmd.colorscheme("solarized-osaka")
+      require("tokyonight").setup(opts)
+      vim.cmd([[colorscheme tokyonight]])
     end,
   },
 }
